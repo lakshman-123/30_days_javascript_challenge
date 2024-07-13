9f24275407787b6867531b105bfe14a650884c4d
lakshman <lr944944@gmail.com>
2024-07-14 00:29:43 +0200

    Day 1: Variables and Datatypes

---
+++ Day01/Day01.js
@@ -0,0 +1,71 @@
+//Day 1:Variables and Data Types
+
+//Tasks/Activities:
+
+//Activity 1:Variable Declaration
+
+//Task 1:Declare a variable using var,assign it to a number,and log the value to the console
+
+var number=10
+console.log(number)
+
+//Task 2:Declare a variable using let,assign it a string, and log the value to the console
+
+let string="day1"
+console.log(string)
+
+//Activity 2:Constant Declaration
+
+//Task 3:Declare a variable using const,assign it to a boolean value,and log the value to the console
+
+const boolean1=true
+console.log(boolean1)
+
+const boolean2=false
+console.log(boolean2)
+
+//Activity 3:Data Types
+
+//Task 4:Create variables of different data types (number,string,boolean,object,array)
+
+let no=10
+console.log(typeof(no))
+
+let stream="hello"
+console.log(typeof(stream))
+
+let boolean=true
+console.log(typeof(boolean))
+
+let object={
+    name:"Bob",
+    age:"56"
+}
+
+console.log(typeof(object))
+
+let array=["hello",1,true]
+console.log(typeof(array))
+
+//Activity 4: Reassigning Variables
+
+//Task 5: Declare a variable using let,assign it an initial value, reassign a new value,and log both values to the console.
+
+let assign=10
+console.log(assign)
+
+assign=11
+console.log(assign)
+
+//Activity 5: understanding the const
+
+//Task 6:Try reassigning a variable declared with const and observe the error
+
+const variable=1
+console.log(variable)
+
+variable=2
+console.log(variable)
+
+
+
