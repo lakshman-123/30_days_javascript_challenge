0399a0a38f63536c1d64833f8cd20a252930cadd
lakshman <lr944944@gmail.com>
2024-07-14 12:24:11 +0200

    Day 2:Operators

---
+++ Day02/Day02.js
@@ -0,0 +1,94 @@
+//Day 2:Operators
+
+//Tasks/Activties
+
+//Activity 1:Arthmetic Operations
+
+//Task 1:Write a program to add two numbers and log the result to the console
+
+let a=10
+let b=2
+let sum=a+b
+console.log(sum)
+
+//TasK 2: Write a program to subtract two numbers and log the result to the console
+
+let sub=a-b
+console.log(sub)
+
+//Task 3:Write a program to multiply two numbers and log the result to the console
+
+let multiply=a*b
+console.log(multiply)
+
+//Task 4:Write a program to divide two numbers and log the result to the console
+
+let divide=a/b
+console.log(divide)
+
+//Task 5:Write a program to find the remainder when one number is divided by another and log the result of the console
+
+let remainder=a%b
+console.log(remainder)
+
+//Activity 2:Assignment Operators
+
+//Task 6:Use the += operator to add a number to a variable and log the result to the console
+
+a+=10
+console.log(a)
+
+//Task 7:Use the -= operature to subtract a number from a variable and log the result to the console
+
+b-=1
+console.log(b)
+
+//Activity 3:Comparison Operators
+
+//Task 8:Write a program to compare two number using > and < and log the result to the console
+
+let compare=a>b 
+console.log(compare)
+
+compare=a<b
+console.log(compare)
+
+//Task 9:Write a program to compare two numbers using >= and <= and log the result to the console
+
+compare=a>=b
+console.log(compare)
+
+compare=a<=b
+console.log(compare)
+
+//Task 10:Write a program to compare two numbers using == and === and log the result to the console
+
+compare=a==b
+console.log(compare) 
+
+compare=a===b
+console.log(compare)
+
+//Activity 4:Logical Operators
+
+//Task 11:Write a program that uses the && operator to combine two conditions and log the result to the console
+
+condition=true&&true
+console.log(condition)
+
+//Task 12:Write a program that uses the || operator to combine two conditions and log the result to the console
+
+condition=true||true
+console.log(condition)
+
+//Task 13:Write a program that uses the ! operator to negate a condition and log the result to the console
+
+condition=!(true)
+console.log(condition)
+
+//Activity 5:Ternary Operator
+
+//Task 14:Write a program that uses the ternary operator to check if a numbber is positive or negative and log the result to the console
+
+ternary=a>0?"positive":"negative"
+console.log(ternary)
